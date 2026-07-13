1aaae643d7cc83bf4f48a2ba06928e855770d0ab
ThinhNguyen <thinhnguyen.02011998@gmail.com>
2026-07-13 19:40:27 +0700

    Resolve merge conflict

--- Java6_ASM/src/main/java/nhomhoinuong/java6_asm/dao/ProductDAO.java
+++ Java6_ASM/src/main/java/nhomhoinuong/java6_asm/dao/ProductDAO.java
@@ -1,13 +1,11 @@
 package nhomhoinuong.java6_asm.dao;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import nhomhoinuong.java6_asm.model.Product;
 
-public interface ProductDAO {
-
-    List<Product> findAll();
-
-    Product findById(Long id);
+@Repository
+public interface ProductDAO extends JpaRepository<Product, Long> {
 
 }
