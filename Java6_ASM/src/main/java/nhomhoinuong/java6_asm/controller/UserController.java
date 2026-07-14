20265c98dec4931263da9ce18d0af02c3e48086b
unknown <hytruong19@gmail.com>
2026-07-14 14:40:43 +0700

    enity

--- Java6_ASM/src/main/java/nhomhoinuong/java6_asm/controller/UserController.java
+++ Java6_ASM/src/main/java/nhomhoinuong/java6_asm/controller/UserController.java
@@ -1,5 +1,30 @@
 package nhomhoinuong.java6_asm.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import nhomhoinuong.java6_asm.bean.User;
+import nhomhoinuong.java6_asm.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // Lấy danh sách User
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    // Lấy User theo Id
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.findById(id).orElse(null);
+    }
 
 }
